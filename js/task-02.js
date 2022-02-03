7072643f9efb4dcc66783efe3d229c7be621449c
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-04 00:43:45 +0200

    task03-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+console.log(ingredientsList);
+
+const elements = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+
+  return itemEl;
+})
+
+ingredientsList.append(...elements);

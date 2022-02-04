b6e28785c5c88e5a615586e870b6ce3e908d283f
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-05 01:02:08 +0200

    no

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,33 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+
+const listEl = document.querySelector('.gallery')
+// console.log(listEl);
+
+const makeImageGallery = image => {
+
+  const items = document.createElement( `li`);
+  itemEl.classList.add("items");
+
+// const imageEl = document.createElement("img")
+//   imageEl.classList.add("image");
+//   imageEl.src = `${images.url}`;
+//   imageEl.alt = `${images.alt}`;
+
+//     // imageEl.src = `https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
+//     // imageEl.alt = `lol`;
+
+
+// itemEl.append(imageEl);
+  
+// console.log(itemEl);
+
+  // return itemEl;
+}
+
+// const elementsImg = images.map(makeImageGallery);
+
+listEl.append(items);

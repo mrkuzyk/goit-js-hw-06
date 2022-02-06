286c9e4ec158eea732ddd98a0d338c18b19638e9
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-07 00:28:01 +0200

    task8-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+const btn = document.querySelector('button')
+// console.log(form);
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        mail, password,
+    };
+
+    if (mail === "" || password === "") {
+        return console.log(`Шановний! Ви забули дещо заповнити`);
+    } else {
+        console.log(`Email: ${mail}, Password: ${password}`);
+        event.currentTarget.reset();
+    };
+}
+
+form.addEventListener('submit', onFormSubmit);

0f82aa8d04e74868d5eb65ab3e67aeab58d1a4b3
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-07 00:36:01 +0200

    update8

--- js/task-08.js
+++ js/task-08.js
@@ -3,22 +3,24 @@ const btn = document.querySelector('button')
 // console.log(form);
 
 const onFormSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); //дефолтні налаштування (при відправці не обновляти)
 
     const formElements = event.currentTarget.elements;
 
     const mail = formElements.email.value;
     const password = formElements.password.value;
 
+    // масив даних на бекенд
     const formData = {
         mail, password,
     };
 
+    // перевірка на відсутність пустих полів
     if (mail === "" || password === "") {
         return console.log(`Шановний! Ви забули дещо заповнити`);
     } else {
         console.log(`Email: ${mail}, Password: ${password}`);
-        event.currentTarget.reset();
+        event.currentTarget.reset(); // при відправці відбувається ресет форми
     };
 }
 

a93c5cfcf33846a91c1e13701ea6c85fe307ad33
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-06 22:52:31 +0200

    task7-done

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const controler = document.querySelector('#font-size-control');
+const textSpan = document.querySelector('#text')
+
+// console.dir(textSpan.style);
+// console.log(controler.value)
+
+// функція змінює "фонтСайз" в залежності від значення "контроллера"
+const onChangeFontSize = () => textSpan.style.fontSize = controler.value + 'px';
+
+controler.addEventListener('change', onChangeFontSize);

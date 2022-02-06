a073882c4ce7f53c8c7274ace244ebc5221cd809
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-07 01:29:54 +0200

    task9-done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const body = document.querySelector('body')
+const btnChange = document.querySelector('.change-color')
+const spanText = document.querySelector('.color')
+
+
+const onChangeBtnColor = (event) => {
+
+  const newColor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  
+  body.style.backgroundColor = newColor;
+  
+  spanText.textContent = newColor;
+};
+
+btnChange.addEventListener('click', onChangeBtnColor);

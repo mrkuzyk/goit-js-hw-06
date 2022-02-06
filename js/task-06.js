ac0dbf955fd6fe1f9594259166e6ffbc592e5e44
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-06 22:19:59 +0200

    task6-done

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('#validation-input');
+
+// колбек функція, яка перевіря кількість введених символів (дата-ленг 
+// в хтмл) після втрати фокусу і відносно цієї перевірки вішає колір
+// бордера на імпут.
+const onInputBlur = (event) => {
+    input.classList.add('invalid'); // вішаю зразу неправильний
+    
+    if (event.currentTarget.value.length == event.currentTarget.dataset.length) {
+        input.classList.add('valid'); //якщо умова збулася то вішаю цей клас
+        input.classList.remove('invalid'); // знімаю клас, якщо умова вірна
+    };
+};
+
+input.addEventListener('blur', onInputBlur);
